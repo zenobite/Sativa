51e4ea32b611dec8fa2bb09ea482c2c65386e14e
Maurana <maurana.hasegawa@gmail.com>
2024-08-20 21:04:33 -0700

    Add Directory Sevice

---
+++ Sativa/Libs/DirectoryServicesCOMException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Sativa.Libs
+{
+    [Serializable]
+    internal class DirectoryServicesCOMException : Exception
+    {
+        public DirectoryServicesCOMException()
+        {
+        }
+
+        public DirectoryServicesCOMException(string message) : base(message)
+        {
+        }
+
+        public DirectoryServicesCOMException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DirectoryServicesCOMException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
